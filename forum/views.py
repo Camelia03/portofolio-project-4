b895e6127df5e865ca3885e8aad67d110da802ec
Camelia03 <camelia_weber@yahoo.com>
2023-07-26 18:38:06 +0000

    Create base template and add bootstrap

--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.views import generic
 
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello, world!')
+    return render(request, 'index.html')
+
+
+class PostList(generic.ListView):
+
+    model = Post
+    template_name = "index.html"
+    paginate_by = 6
+    queryset = Post.objects.all()
