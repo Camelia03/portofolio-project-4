b5ad7fe0c69fbe6cebd9012416d929b7d0544474
Camelia03 <72148154+Camelia03@users.noreply.github.com>
2023-08-09 21:07:56 +0200

    Integrate crispy forms

--- go_global/settings.py
+++ go_global/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'crispy_forms',
+    'crispy_bootstrap5',
     'forum',
 ]
 
@@ -75,7 +76,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-CRIPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
