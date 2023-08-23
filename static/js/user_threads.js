4b22710e52ec0b6ed4502510aa0d0266d9ca2b3a
Camelia03 <72148154+Camelia03@users.noreply.github.com>
2023-08-23 19:59:15 +0200

    Add minor changes to js file in order to improve validation results

--- static/js/user_threads.js
+++ static/js/user_threads.js
@@ -1,13 +1,13 @@
-const deleteConfirmModal = document.getElementById('deleteConfirmModal')
+const deleteConfirmModal = document.getElementById('deleteConfirmModal');
 if (deleteConfirmModal) {
   deleteConfirmModal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
-    const button = event.relatedTarget
+    const button = event.relatedTarget;
     // Extract info from data-bs-thread-id
-    const threadId = button.getAttribute('data-bs-thread-id')
+    const threadId = button.getAttribute('data-bs-thread-id');
 
     // Update the form's input value
-    const threadIdInput = deleteConfirmModal.querySelector('#threadId')
-    threadIdInput.value = threadId
-  })
+    const threadIdInput = deleteConfirmModal.querySelector('#threadId');
+    threadIdInput.value = threadId;
+  });
 }
