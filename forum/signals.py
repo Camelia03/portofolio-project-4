6376bdf7b2867504270e927fd88fb95de90ee2b8
Camelia03 <72148154+Camelia03@users.noreply.github.com>
2023-08-26 14:04:42 +0200

    Add comments to signals

--- forum/signals.py
+++ forum/signals.py
@@ -7,16 +7,22 @@ from forum.models import Profile, Thread
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """Create a profile when a user is created"""
+
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """Save a profile when a user is saved"""
+
     instance.profile.save()
 
 
 @receiver(pre_save, sender=Thread)
 def update_updated_on(sender, instance, **kwargs):
+    """Set updated_on to now when the user is updated"""
+
     if instance.pk is not None:
         instance.updated_on = timezone.now()
