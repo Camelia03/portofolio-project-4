18fe4c0011cc02a16880c038a002897e2b188f47
Camelia03 <72148154+Camelia03@users.noreply.github.com>
2023-08-26 13:56:35 +0200

    Add comments to forms

--- forum/forms.py
+++ forum/forms.py
@@ -6,12 +6,16 @@ from django.core.exceptions import ValidationError
 
 
 class ThreadForm(forms.ModelForm):
+    """Form for creating a thread"""
+
     class Meta:
         model = Thread
         fields = ['channel', 'title', 'content', 'image']
 
 
 class ReplyForm(forms.ModelForm):
+    """Form for creating a reply"""
+
     class Meta:
         model = Reply
         fields = ['content']
@@ -21,6 +25,7 @@ class ReplyForm(forms.ModelForm):
 
 
 class UpdateUserForm(forms.ModelForm):
+    """Form for updating the user part of the profile"""
 
     class Meta:
         model = User
@@ -28,6 +33,7 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
+    """Form for updating the profile"""
 
     class Meta:
         model = Profile
@@ -35,6 +41,8 @@ class UpdateProfileForm(forms.ModelForm):
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """Form for the signup page"""
+
     email = forms.EmailField(label='Email', required=True)
 
     class Meta:
@@ -42,9 +50,16 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
     def clean(self):
+        """Clean the form and ensure the email is unique"""
+
         cleaned_data = super().clean()
         email = cleaned_data.get('email')
+
+        # Find user with this email
         if User.objects.filter(email=email).exists():
+            # Return a validation error for the email field
+            message = 'This email address is already assigned to another user'
             raise ValidationError(
-                {'email': 'This email address is already assigned to another user'})
+                {'email': message}
+            )
         return cleaned_data
