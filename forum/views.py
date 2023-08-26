8f277c5e9188f4e40a2f1a5a9bfc4f75be99cebd
Camelia03 <72148154+Camelia03@users.noreply.github.com>
2023-08-26 14:33:46 +0200

    Add comments to views

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Thread, Upvote, Downvote, Channel, Reply
 from django.views import View
-from .forms import ThreadForm, ReplyForm, UpdateProfileForm, UpdateUserForm, CustomUserCreationForm
+from .forms import ThreadForm, ReplyForm, UpdateProfileForm, UpdateUserForm, \
+    CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -15,11 +16,10 @@ from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 
 
-# Create your views here.
-
-
 @method_decorator(login_required, name='dispatch')
 class Index(ListView):
+    """List all threads or channel threads"""
+
     model = Thread
     template_name = 'index.html'
     paginate_by = 5
@@ -53,11 +53,21 @@ class Index(ListView):
         return threads.order_by(order_by)
 
     def get_context_data(self, **kwargs):
+        """Create a context"""
+
+        # Get existing context
         context = super().get_context_data(**kwargs)
 
+        # Set the order by from the url param or desc created_on as default
         context['order_by'] = self.request.GET.get('order_by') or '-created_on'
+
+        # Get all channels
         context['channels'] = Channel.objects.all()
+
+        # Get the total number of threads
         context['total_threads_nr'] = Thread.objects.count()
+
+        # Show the sidebar
         context['has_sidebar'] = True
 
         # Attempts to add a current channel if it's part of the url
@@ -72,16 +82,25 @@ class Index(ListView):
 
 @method_decorator(login_required, name='dispatch')
 class ThreadDetail(View):
+    """View to display the details of a thread"""
 
     def get(self, request, pk, *args, **kwargs):
+        """Show the thread details page"""
+
+        # Look for the threads from the url
         thread = get_object_or_404(Thread, pk=pk)
+
+        # Get all the replies
         replies = thread.replies.all()
+
+        # See wether the logged in user has up/downvoted the thread
         user_upvotes = Upvote.objects.filter(user=request.user, thread=thread)
         user_downvotes = Downvote.objects.filter(
             user=request.user, thread=thread)
         user_upvotes_thread = user_upvotes.count() != 0
         user_downvotes_thread = user_downvotes.count() != 0
 
+        # Render the template
         return render(
             request,
             "thread_detail.html",
@@ -95,65 +114,89 @@ class ThreadDetail(View):
         )
 
     def post(self, request, pk, *args, **kwargs):
+        """Create a reply"""
+
+        # Get the thread from the url
         thread = get_object_or_404(Thread, pk=pk)
 
+        # Create the reply
         reply_form = ReplyForm(request.POST)
         if reply_form.is_valid():
             reply = reply_form.save(commit=False)
             reply.thread = thread
             reply.user = request.user
             reply.save()
+
+        # Redirect to the thread detail page
         return redirect('thread_detail', pk=pk)
 
 
 @method_decorator(login_required, name='dispatch')
 class ReplyDelete(View):
+    """View for deleting a reply"""
 
     def post(self, request):
+        """Delete a given reply"""
+
         reply_id = request.POST.get('reply_id')
         thread_id = request.POST.get('thread_id')
 
+        # Ensure the logged in user owns the reply
         reply = get_object_or_404(Reply, pk=reply_id)
         if request.user.id == reply.user.id:
             reply.delete()
         else:
             raise PermissionDenied()
+
+        # Add a success message
         messages.success(request,  'The reply has been deleted successfully.')
+
+        # Redirect to the thread details page
         return redirect('thread_detail', pk=thread_id)
 
 
 @method_decorator(login_required, name='dispatch')
 class ReplyEdit(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    """View for editing a reply"""
+
     model = Reply
     template_name = "reply_edit.html"
     success_message = "Your reply has been edited successfully!"
     fields = ['content']
 
-    # Check if the logged in user owns the reply
-
     def test_func(self):
+        """Check if the logged in user owns the reply"""
+
         reply = self.get_object()
         return self.request.user.id == reply.user.id
 
     def get_success_url(self):
-        # Customize the redirect URL
+        """Customize the redirect URL"""
+
         return reverse_lazy(
             'thread_detail', kwargs={'pk': self.object.thread.id})
 
 
 @method_decorator(login_required, name='dispatch')
 class ThreadAdd(View):
+    """View for adding a thread"""
+
     def get(self, request):
+        """Show the add thread form"""
+
         form = ThreadForm()
 
         context = {
             "form": form,
         }
+
+        # If a channel was selected - pre-populate the channel field
         channel_name = request.GET.get('channel')
         if channel_name:
             form.fields['channel'].initial = get_object_or_404(
                 Channel, slug=channel_name)
 
+        # Render the add thread form
         return render(
             request,
             "thread_add.html",
@@ -161,8 +204,11 @@ class ThreadAdd(View):
         )
 
     def post(self, request):
+        """Create a thread"""
+
         form = ThreadForm(request.POST, request.FILES)
 
+        # Check if the form is valid
         if form.is_valid() is False:
             return render(
                 request,
@@ -172,14 +218,21 @@ class ThreadAdd(View):
                 }
             )
 
+        # Create the thread
         thread = form.save(commit=False)
         thread.user = request.user
         thread.save()
+
+        # Add a success message
         messages.success(request, 'Your thread was created successfully.')
+
+        # Redirect to the list of threads
         return redirect('index')
 
 
 class SignUpView(SuccessMessageMixin, CreateView):
+    """View for registering a new user"""
+
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
@@ -189,6 +242,8 @@ class SignUpView(SuccessMessageMixin, CreateView):
 # Upvote and Downvote views
 @login_required
 def upvote_thread(request, pk):
+    """View for upvoting a thread"""
+
     thread = get_object_or_404(Thread, pk=pk)
     user = request.user
 
@@ -213,6 +268,8 @@ def upvote_thread(request, pk):
 
 @login_required
 def downvote_thread(request, pk):
+    """View for downvoting a thread"""
+
     thread = get_object_or_404(Thread, pk=pk)
     user = request.user
 
@@ -235,21 +292,30 @@ def downvote_thread(request, pk):
     return redirect('thread_detail', pk=pk)
 
 
-# Profile
 @login_required
 def profile(request):
+    """View for displaying the user profile"""
+
     return render(request, 'profile.html')
 
 
 @login_required
 def view_profile(request, username):
+    """View for displaying the public user profile"""
+
     profile_user = get_object_or_404(User, username=username)
-    return render(request, 'public_profile.html', {'public_user': profile_user})
+    return render(
+        request, 'public_profile.html', {'public_user': profile_user}
+    )
 
 
 @method_decorator(login_required, name='dispatch')
 class EditProfile(View):
+    """View for editing the profile of a user"""
+
     def get(self, request):
+        """Display the edit profile form"""
+
         user_form = UpdateUserForm(instance=request.user)
         profile_form = UpdateProfileForm(instance=request.user.profile)
 
@@ -260,15 +326,25 @@ class EditProfile(View):
         return render(request, 'edit_profile.html', context)
 
     def post(self, request):
+        """Edit a profile"""
+
+        # Extract forms from the request
         user_form = UpdateUserForm(request.POST, instance=request.user)
         profile_form = UpdateProfileForm(
             request.POST, request.FILES, instance=request.user.profile)
+
+        # Make sure the forms are valid
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+
+            # Create a success message
             messages.success(request, 'Your profile was updated successfully.')
+
+            # Redirect to the user profile
             return redirect('user_profile')
 
+        # Show the form with the validation errors
         context = {
             'user_form': user_form,
             'profile_form': profile_form
@@ -278,7 +354,12 @@ class EditProfile(View):
 
 @method_decorator(login_required, name='dispatch')
 class UserThreads(View):
+    """View for showing the threads of a user"""
+
     def get(self, request):
+        """Show the list of threads of a user"""
+
+        """Get all threads filtered by the logged in user"""
         threads = Thread.objects.filter(
             user=request.user).order_by('-created_on')
         context = {
@@ -289,44 +370,63 @@ class UserThreads(View):
 
 @method_decorator(login_required, name='dispatch')
 class ThreadEdit(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    """View for editing a thread"""
+
     model = Thread
     template_name = "thread_edit.html"
     form_class = ThreadForm
     success_message = "Your thread has been edited successfully!"
 
-    # Check if the logged in user owns the thread
-
     def test_func(self):
+        """Check if the logged in user owns the thread"""
+
         thread = self.get_object()
         return self.request.user.id == thread.user.id
 
     def get_success_url(self):
-        # Customize the redirect URL
+        """Customize the redirect URL"""
+
         return reverse_lazy('thread_detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(login_required, name='dispatch')
 class ThreadDelete(View):
+    """View for deleting a thread"""
 
     def post(self, request):
+        """Delete a thread"""
+
         thread_id = request.POST.get('thread_id')
         thread = get_object_or_404(Thread, pk=thread_id)
+
+        # Make sure the user owns the thread
         if request.user.id == thread.user.id:
+            # Delete the thread
             thread.delete()
         else:
             raise PermissionDenied()
+
+        # Create a success message
         messages.success(request,  'The thread has been deleted successfully.')
+
+        # Redirect to the list of user's threads
         return redirect('user_threads')
 
 
 @method_decorator(login_required, name='dispatch')
 class ThreadSearch(ListView):
+    """View for searching threads"""
+
     model = Thread
     template_name = 'search.html'
     paginate_by = 5
     context_object_name = 'threads'
 
     def get_queryset(self):
+        """
+        Get all threads that contain the given keyword in the title or content
+        """
+
         keyword = self.request.GET.get('keyword')
 
         if not keyword:
@@ -339,9 +439,11 @@ class ThreadSearch(ListView):
         return Thread.objects.filter(query).order_by('title')
 
     def get_context_data(self, **kwargs):
+        """Create the context"""
+
         context = super().get_context_data(**kwargs)
 
-        # Adding a custom value to the context
+        # Adding the keyword to the context
         keyword = self.request.GET.get('keyword')
         context['keyword'] = keyword
 
